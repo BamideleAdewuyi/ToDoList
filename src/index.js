8f37b3c6775a8062fe8fb894fb52dd887264f3e9
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-01-23 13:56:16 +0000

    index.js

--- src/index.js
+++ src/index.js
@@ -2,27 +2,50 @@ class Projects {
     constructor() {
         this.allProjects = [];
     }
-    addToProject(ToDo, array) {
-        array.push(ToDo)
+
+    createProject(projectName) {
+        const project = new Projects(projectName);
+        this.allProjects.push(project);
+        return project;
     }
+
 }
 
-class ToDo {
-    constructor(title, description, dueDate, priority) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
-};
 
-function CreateToDo(title, description, dueDate, priority) {
-    return new ToDo(title, description, dueDate, priority);
-};
 
-const Project = new Projects;
-const testItem = CreateToDo("Cook", "Cooking", "Today", 1);
 
-Project.addToProject(testItem, Project.allProjects);
 
-console.log(Project)
+
+// class Projects {
+//     constructor() {
+//         this.allProjects = [];
+//     }
+
+//     createProject(project) {
+//         this.project = [];
+//     }
+//     addToProject(ToDo, array) {
+//         array.push(ToDo)
+//     }
+// }
+
+// class ToDo {
+//     constructor(title, description, dueDate, priority) {
+//         this.title = title;
+//         this.description = description;
+//         this.dueDate = dueDate;
+//         this.priority = priority;
+//     }
+// };
+
+// function CreateToDo(title, description, dueDate, priority) {
+//     return new ToDo(title, description, dueDate, priority);
+// };
+
+// const Project = new Projects;
+// // const testItem = CreateToDo("Cook", "Cooking", "Today", 1);
+
+// // Project.addToProject(testItem, Project.allProjects);
+
+// // console.log(Project)
+// Project.createProject(cuisine)

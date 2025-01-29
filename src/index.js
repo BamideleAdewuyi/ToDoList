8902649098e220ab04af493e9a1909134a49f6ab
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-01-29 17:05:25 +0000

    index.js: Can now create a project manager and default project which empty array of toDos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import "./style.css";
+
+class ProjectManager {
+    constructor() {
+        this.projects = {};
+        this.defaultProject = new Project("Default");
+        this.addProject(this.defaultProject);
+    }
+
+    createProject(title) {
+        const newProject = new Project(title);
+        return newProject;
+    }
+
+    addProject(project) {
+        this.projects[project.title] = [];
+        return;
+    }
+}
+
+class Project {
+    constructor(title) {
+        this.title = title;
+        this.toDos = [];
+    }
+}
+
+const projectManager = new ProjectManager();
+
+console.log(projectManager.projects)

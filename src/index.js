f2092bdb97d91caf24ec80bc4064ff8f1b3392a6
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-03-02 22:01:47 +0000

    index.js: Created const formHeading

--- src/index.js
+++ src/index.js
@@ -98,6 +98,7 @@ class ScreenController {
 
     createProjectForm() {
         const projectForm = document.createElement("dialog");
+        const formHeading = document.createElement("h3");
         return;
     }
 }

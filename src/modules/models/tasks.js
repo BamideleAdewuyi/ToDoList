c8fddc56504624865d024444076b27ea333cecb3
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-03-04 18:38:53 +0000

    tasks.js: Exported Task

--- src/modules/models/tasks.js
+++ src/modules/models/tasks.js
@@ -1,8 +1,10 @@
-class Tasks {
+class Task {
     constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
     }
-}
+};
+
+export default Task;

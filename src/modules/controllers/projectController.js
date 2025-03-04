dfb71463dcb51ca7276ec713779f709db094579e
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-03-04 23:32:52 +0000

    projectController.js: Added addProject method

--- src/modules/controllers/projectController.js
+++ src/modules/controllers/projectController.js
@@ -4,4 +4,9 @@ class ProjectController{
     constructor() {
         this.projects = [];
     }
+
+    addProject(title) {
+        const project = new Project(title);
+        return project;
+    }
 }

e5b871d45a9e020762cbae5475c944050f073f89
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-03-05 18:54:33 +0000

    taskController.js: Wrote createTask method

--- src/modules/controllers/taskController.js
+++ src/modules/controllers/taskController.js
@@ -4,4 +4,9 @@ class TaskController{
     constructor() {
         this.tasks = [];
     }
+
+    createTask(title, description, dueDate, priority) {
+        const newTask = new Task(title, description, dueDate, priority)
+        return newTask;
+    }
 }

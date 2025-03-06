152494c1cf1b3cfd8020b0979843aa98fa5a8da4
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-03-06 00:26:50 +0000

    appController.js: Wrote constructor for AppController class

--- src/modules/controllers/appController.js
+++ src/modules/controllers/appController.js
@@ -2,5 +2,8 @@ import TaskController from "./taskController";
 import ProjectController from "./projectController";
 
 class AppController{
-    
+    constructor() {
+        this.taskController = new TaskController();
+        this.projectController = new ProjectController();
+    };
 }

ff9a559a0dafa3be91d18d4ab2da00ad3f75305b
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-03-09 20:28:38 +0000

    appView.js: Added AppView constructor

--- src/modules/views/appView.js
+++ src/modules/views/appView.js
@@ -2,5 +2,8 @@ import ProjectView from "./projectView";
 import TaskView from "./taskView";
 
 class AppView {
-    
+    constructor() {
+        this.projectView = new ProjectView();
+        this.taskView = new TaskView();
+    }
 }

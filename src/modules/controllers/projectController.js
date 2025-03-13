e9cf12782763a62dc9781122a29bf28b9bc19bbd
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-03-13 02:56:36 +0000

    projectController.js: Pasted refreshProjects method from appController.js

--- src/modules/controllers/projectController.js
+++ src/modules/controllers/projectController.js
@@ -3,28 +3,39 @@ import Project from "../models/projects";
 class ProjectController{
     constructor() {
         this.projects = [];
-    }
+    };
 
     createProject(title) {
         const project = new Project(title);
         return project;
-    }
+    };
 
     addProject(project) {
         this.projects.push(project);
         return;
-    }
+    };
 
     getProject(title) {
         const project = this.projects.find((obj) => obj.title === title);
         return project;
-    }
+    };
 
     clearProjects() {
         for (const project of this.projects) {
             project.tasks = [];
         }
-    }
+    };
+
+    refreshProjects(projectController, taskController) {
+        this.projectController.clearProjects();
+        for (const task of this.taskController.tasks) {
+            for (const project of this.projectController.projects) {
+                if (task.project === project.title) {
+                    this.taskController.addTask(task, project.tasks)
+                }
+            }
+        }
+    };
 
 };
 

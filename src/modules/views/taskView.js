c20d634759d4f6820b85c2229715c2ccc4fa7bed
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-03-14 16:05:04 +0000

    taskView.js: Wrote createTaskDiv method

--- src/modules/views/taskView.js
+++ src/modules/views/taskView.js
@@ -6,7 +6,21 @@ class TaskView {
     };
 
     createTaskDiv(task) {
-        
+        const taskDiv = document.createElement("div");
+        const taskTitle = document.createElement("h3");
+        taskTitle.textContent = task.title;
+        const taskDescription = document.createElement("p");
+        taskDescription.textContent = task.description;
+        const taskDueDate = document.createElement("p");
+        taskDueDate.textContent = task.dueDate;
+        const taskPriority = document.createElement("p");
+        taskPriority.textContent = task.priority;
+        const closeTask = document.createElement("button");
+        closeTask.setAttribute("type", "button");
+        closeTask.classList.add("closeTaskButton");
+        closeTask.textContent = "X";
+        taskDiv.append(taskTitle, taskDescription, taskDueDate, taskPriority, closeTask);
+        return taskDiv;
     }
 }
 

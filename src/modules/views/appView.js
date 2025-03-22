62989fa398180e6e36813de12106774b15e6f240
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-03-22 00:18:12 +0000

    appView.js: Removed createTaskForm method

--- src/modules/views/appView.js
+++ src/modules/views/appView.js
@@ -39,39 +39,6 @@ class AppView {
         });
     };
 
-    createTaskForm() {
-        const dialog = document.createElement("dialog");
-        dialog.classList.add("createTaskForm")
-        const form = document.createElement("form");
-        form.innerHTML = `<form action="#" class="newToDoForm">
-                         <h3>New ToDo</h3>
-                         <label for="title">Title</label>
-                         <input type="text" placeholder="Title" name="title" id="title" required>
-                         <label for="description">Description</label>
-                         <input type="text" placeholder="Description" name="description" id="description" required>
-                         <label for="dueDate">Due Date</label>
-                         <input type="date" name="dueDate" id="dueDate" required>
-                         <div class="selectPriority">
-                             <p>Select priority</p>
-                             <label for="low">Low</label>
-                             <input type="radio" id="low" name="priority" value="low" required/>
-                             <label for="medium">Medium</label>
-                             <input type="radio" id="medium" name="priority" value="medium"/>
-                             <label for="high">High</label>
-                             <input type="radio" id="high" name="priority" value="high"/>
-                         </div>
-                         <label for="project">Select project</label>
-                         <select name="project" id="project">
- 
-                         </select>
-                         <button class="submit" id="createToDo">Add ToDo</button>
-                         <button type="button" class="closeToDoFormButton" autofocus>Close</button>
-                     </form>
-        `
-        dialog.append(form);
-        return dialog;
-    };
-
     createProjectForm() {
         const dialog = document.createElement("dialog");
         const form = document.createElement("form");

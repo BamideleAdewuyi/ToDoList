ab2cf4a85d65ad0adfc054cdc17928cc7d6ccdf1
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-03-23 22:09:27 +0000

    projectView.js: Changed projectForm id

--- src/modules/views/projectView.js
+++ src/modules/views/projectView.js
@@ -59,8 +59,8 @@ class ProjectView {
         const form = document.createElement("form");
         form.innerHTML = `<form action="#" class="newProjectForm">
                         <h3>New Project</h3>
-                        <label for="title">Title</label>
-                        <input type="text" placeholder="Title" name="title" id="title" required>
+                        <label for="projectTitle">Title</label>
+                        <input type="text" placeholder="Title" name="projectTitle" id="projectTitle" required>
                         <button class="submit" id="createProject">Add Project</button>
                         <button type="button" class="closeProjectFormButton" autofocus>Close</button>
                         </form>
@@ -77,6 +77,8 @@ class ProjectView {
         this.closeProjectFormButton.addEventListener("click", () => {
             this.closeProjectForm();
         })
+
+        
     };
 
     openProjectForm() {

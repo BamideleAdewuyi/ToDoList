ffef5ed9bddaa6ee308d92260245f867f29eb9bf
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-04-11 16:16:07 +0100

    projectController.js: Added storageAvailable method

--- src/modules/controllers/projectController.js
+++ src/modules/controllers/projectController.js
@@ -6,6 +6,25 @@ class ProjectController{
         this.id = 1;
     };
 
+    storageAvailable(type) {
+        let storage;
+        try {
+          storage = window[type];
+          const x = "__storage_test__";
+          storage.setItem(x, x);
+          storage.removeItem(x);
+          return true;
+        } catch (e) {
+          return (
+            e instanceof DOMException &&
+            e.name === "QuotaExceededError" &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage &&
+            storage.length !== 0
+          );
+        }
+    }
+
     createProject(title, id) {
         const project = new Project(title, id);
         this.id += 1;

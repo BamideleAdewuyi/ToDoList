ba2303c07448cb550baf42f9533daf921cadd201
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-04-17 13:46:50 +0100

    projectController.js: Added same removeItem method as in taskController.js

--- src/modules/controllers/projectController.js
+++ src/modules/controllers/projectController.js
@@ -75,6 +75,14 @@ class ProjectController{
         }
     };
 
+    removeItem(arr, item) {
+        const index = arr.indexOf(item);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+        return arr;
+    }
+
     deleteProject(projectId) {
 
     };
